2945ced7a977d90aac030266199cf7f6ffa2c293
Omkar Kolhe <61349460+omkar2306@users.noreply.github.com>
2021-07-23 23:54:25 -0700

    Add files via upload

---
+++ Virtual Function/virtual_1.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<string.h>
+class person
+{
+private:
+  char name[20];
+  int age;
+public:
+  person(const char* name="",int age=0)
+  {
+    strcpy(this->name,name);
+    this->age=age;
+
+  }
+  void setName(const char* name="")
+  {
+    strcpy(this->name,name);
+  }
+  void setAge(int age=0)
+  {
+     this->age=age;
+  }
+
+  const char* getName()
+  {
+    return this->name;
+
+  }
+  int getAge()
+  {
+    return this->age;
+  }
+  virtual void display()
+  {
+    std::cout<<"NAME:-"<<this->name<<std::endl<<"AGE:-"<<this->age<<std::endl;
+  }
+};
+class employee:public person
+{
+private:
+  int id;
+  float salary;
+public:
+  employee(const char* name="", int age=0, int id=0, float salary=0.0f):person(name,age)
+  {
+    this->id=id;
+    this->salary=salary;
+
+  }
+  void setId(int id=0)
+  {
+    this->id=id;
+  }
+  void setSalary(float salary=0.0f)
+  {
+    this->salary=salary;
+  }
+  int getId()
+  {
+    return this->id;
+  }
+  float getSalary()
+  {
+    return this->salary;
+  }
+  void display()
+  {
+    person::display();
+    std::cout<<"ID:-"<<this->id<<std::endl<<"SALARY:-"<<this->salary<<std::endl;
+  }
+
+};
+int main()
+{
+employee e1("MAHESH",19,3183,500000);
+person* p1=NULL;
+p1=&e1;
+p1->display();
+return 0;
+}
---
+++ Virtual Function/virtual_a.cpp
@@ -0,0 +1,136 @@
+#include<iostream>
+#include<string.h>
+class employee
+{
+private:
+  char name[10];
+  int age;
+  int id;
+protected:
+  float salary;
+public:
+  employee(const char* name="", int age=0, int id=0, float salary=0.0f)
+  {
+    strcpy(this->name,name);
+    this->age=age;
+    this->id=id;
+    this->salary=salary;
+  }
+    char* getName()
+    {
+      return this->name;
+    }
+    int getAge()
+    {
+      return this->age;
+
+    }
+    int getId()
+    {
+      return this->id;
+    }
+    float getSalary()
+    {
+      return this->salary;
+    }
+    virtual float cal_salary()
+    {
+      return this->salary;
+    }
+    virtual void display()
+    {
+      std::cout<<"NAME:"<<this->name<<std::endl<<"AGE:"<<this->age<<std::endl<<"ID:"<<this->id<<std::endl<<"SALARY:"<<this->salary<<std::endl;
+    }
+};
+
+class manager:public employee
+{
+public:
+  float bonus;
+public:
+  manager(const char* name="", int age=0, int id=0, float salary=0.0f,int bonus=0):employee(name,age,id,salary)
+  {
+  this->bonus=bonus;
+  }
+ float getBonus()
+ {
+   return this->bonus;
+ }
+ float cal_salary()
+ {
+   return (this->salary+this->bonus);
+ }
+ void display()
+ {
+  employee::display();
+  std::cout<<"BONUS:"<<this->bonus<<std::endl;
+ }
+
+};
+class saleman:public employee
+{
+private:
+  float insentive;
+public:
+  saleman(const char* name="", int age=0, int id=0, float salary=0.0f,int insentive=0):employee(name,age,id,salary)
+  {
+  this->insentive=insentive;
+  }
+ int getInsentive()
+ {
+   return this->insentive;
+ }
+ float cal_salary()
+ {
+ return(this->salary+this->insentive);
+}
+ void display()
+ {
+  employee::display();
+  std::cout<<"INSENTIVE:"<<this->insentive<<std::endl;
+ }
+
+};
+float total_sal(employee* arr[],int n)
+{
+  int i;
+float total_s=0.0f;
+for(i=0;i<n;i++)
+{
+total_s=total_s+arr[i]->getSalary();
+}
+return total_s;
+}
+float total_inco(employee* arr[],int n)
+{
+  int i;
+float total_income=0.0f;
+for(i=0;i<n;i++)
+{
+total_income=total_income+arr[i]->cal_salary();
+}
+return total_income;
+}
+int main()
+{
+  int i;
+  employee* arr[6];
+
+  arr[0]=new employee("MAHESH",19,33,50000.0f);
+  arr[1]=new manager("KIRAN",20,43,50000.0f,5000);
+  arr[2]=new saleman("YOGESH",18,53,50000.0f,2000);
+  arr[3]=new employee("ADITYA",19,34,50000.0f);
+  arr[4]=new manager("AJIT",21,44,50000.0f,5000);
+  arr[5]=new saleman("SACHIN",22,54,50000.0f,2000);
+
+  for(i=0;i<6;i++)
+  {
+    arr[i]->display();
+  }
+
+  float total_salary=total_sal(arr,6);
+  std::cout<<"TOTAL SALARY:"<<total_salary<<std::endl;
+
+  float total_income=total_inco(arr,6);
+  std::cout<<"TOTAL INCOME:"<<total_income<<std::endl;
+}
